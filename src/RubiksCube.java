1d0bd133f9a2b67591f3e45dc2311066af14aa0c
Timothy Dietsch <dietschtm18@gcc.edu>
2021-03-09 15:59:52 -0500

    RubiksCube class creation and package declaration
    Created the RubiksCube class with a bunch of dummy methods.
    
    Set the package to src, because VSCode wouldn't stop screaming at me about it.

---
+++ src/RubiksCube.java
@@ -0,0 +1,63 @@
+package src;
+
+public class RubiksCube {
+    
+    //TODO Create a data structure for the cube faces
+
+    public RubiksCube() {
+
+    }
+
+    //Moves for a Rubiks Cube
+
+    public void move_U() {
+
+    }
+
+    public void move_D() {
+
+    }
+
+    public void move_R() {
+
+    }
+
+    public void move_L() {
+
+    }
+
+    public void move_F() {
+
+    }
+
+    public void move_B() {
+
+    }
+
+    //Inverse moves, p stands for prime
+
+    public void move_Up() {
+
+    }
+
+    public void move_Dp() {
+
+    }
+
+    public void move_Rp() {
+
+    }
+
+    public void move_Lp() {
+
+    }
+
+    public void move_Fp() {
+
+    }
+
+    public void move_Bp() {
+        
+    }
+
+}
