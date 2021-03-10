ef0f2cf6d32d8ae5eba91f9360070629d5bfcd3c
Timothy Dietsch <dietschtm18@gcc.edu>
2021-03-10 18:57:38 -0500

    Completed move F' in RubiksCube

--- src/pack/RubiksCube.java
+++ src/pack/RubiksCube.java
@@ -19,54 +19,87 @@ public class RubiksCube {
 
     //Moves for a Rubiks Cube
 
-    public void move_U() {
-
-    }
-
-    public void move_D() {
-
-    }
-
-    public void move_R() {
-
-    }
-
+    //index 0
     public void move_L() {
-
+    	
     }
-
+    
+    //index 1
     public void move_F() {
-
+    	
     }
-
+    
+    //index 2
+    public void move_R() {
+    	
+    }
+    
+    //index 3
     public void move_B() {
-
+        
+    }
+    
+    //index 4
+    public void move_U() {
+    	
+    }
+    
+    //index 5
+    public void move_D() {
+    	
     }
 
     //Inverse moves, p stands for prime
-
-    public void move_Up() {
-
-    }
-
-    public void move_Dp() {
-
-    }
-
-    public void move_Rp() {
-
-    }
-
+    
+    //index 0
     public void move_Lp() {
-
+    	rotateFace(0, false);
     }
-
+    
+    //index 1
+    //4 -> 0 -> 5 -> 2
     public void move_Fp() {
-
+    	rotateFace(1, false);
+    	
+    	char temp1 = cube[4][6];
+    	char temp2 = cube[4][7];
+    	char temp3 = cube[4][8];
+    	
+    	cube[4][6] = cube[2][0];
+    	cube[4][7] = cube[2][3];
+    	cube[4][8] = cube[2][6];
+    	
+    	cube[2][0] = cube[5][2];
+    	cube[2][3] = cube[5][1];
+    	cube[2][6] = cube[5][0];
+    	
+    	cube[5][2] = cube[0][8];
+    	cube[5][1] = cube[0][5];
+    	cube[5][0] = cube[0][2];
+    	
+    	cube[0][8] = temp1;
+    	cube[0][5] = temp2;
+    	cube[0][2] = temp3;
     }
-
+    
+    //index 2
+    public void move_Rp() {
+    	rotateFace(2, false);
+    }
+    
+    //index 3
     public void move_Bp() {
-        
+    	rotateFace(3, false);
+    }
+    
+    //index 4
+    public void move_Up() {
+    	rotateFace(4, false);
+    }
+    
+    //index 5
+    public void move_Dp() {
+    	rotateFace(5, false);
     }
     
     @Override
@@ -86,7 +119,7 @@ public class RubiksCube {
     
     //0 -> 2 -> 8 -> 6
     //1 -> 5 -> 7 -> 4
-    private void rotateSide(int side, boolean clockwise) {
+    private void rotateFace(int side, boolean clockwise) {
     	
     	char temp1 = cube[side][0];
     	char temp2 = cube[side][1];
