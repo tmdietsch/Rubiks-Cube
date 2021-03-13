3edbfd9d2aed8842243aa043b416e01dd4872550
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-03-13 17:42:35 -0500

    printCube()

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -3,5 +3,7 @@ package pack;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\nTesting\n");
+        RubiksCube cube = new RubiksCube();
+        cube.printCube();
     }
 }
--- src/pack/RubiksCube.java
+++ src/pack/RubiksCube.java
@@ -374,4 +374,25 @@ public class RubiksCube {
     	
     }
 
+    public void printCube() {
+    	System.out.println("      |" + cube[4][0] + "|" + cube[4][1] + "|" + cube[4][2] + "|");
+    	System.out.println("      |" + cube[4][3] + "|" + cube[4][4] + "|" + cube[4][5] + "|");
+    	System.out.println("      |" + cube[4][6] + "|" + cube[4][7] + "|" + cube[4][8] + "|");
+    	System.out.println("|"+ cube[0][0] + "|" + cube[0][1] + "|" + cube[0][2] + "|" + 
+    			cube[1][0] + "|" + cube[1][1] + "|" + cube[1][2] + "|" +
+    			cube[2][0] + "|" + cube[2][1] + "|" + cube[2][2] + "|" +
+    			cube[3][0] + "|" + cube[3][1] + "|" + cube[3][2] + "|");
+    	System.out.println("|"+ cube[0][3] + "|" + cube[0][4] + "|" + cube[0][5] + "|" + 
+    			cube[1][3] + "|" + cube[1][4] + "|" + cube[1][5] + "|" +
+    			cube[2][3] + "|" + cube[2][4] + "|" + cube[2][5] + "|" +
+    			cube[3][3] + "|" + cube[3][4] + "|" + cube[3][5] + "|");
+    	System.out.println("|"+ cube[0][0] + "|" + cube[0][1] + "|" + cube[0][2] + "|" + 
+    			cube[1][6] + "|" + cube[1][7] + "|" + cube[1][8] + "|" +
+    			cube[2][6] + "|" + cube[2][7] + "|" + cube[2][8] + "|" +
+    			cube[3][6] + "|" + cube[3][7] + "|" + cube[3][8] + "|");
+    	System.out.println("      |" + cube[5][0] + "|" + cube[5][1] + "|" + cube[5][2] + "|");
+    	System.out.println("      |" + cube[5][3] + "|" + cube[5][4] + "|" + cube[5][5] + "|");
+    	System.out.println("      |" + cube[5][6] + "|" + cube[5][7] + "|" + cube[5][8] + "|");
+    }
+    
 }
