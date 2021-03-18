ffcd0307fc148416b33c9542d14f6e3c6e70194a
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-03-17 23:58:54 -0400

    Filled in Node class

--- src/pack/IterativeDepthFirstSearch.java
+++ src/pack/IterativeDepthFirstSearch.java
@@ -2,4 +2,6 @@ package pack;
 
 public class IterativeDepthFirstSearch {
 
+	
+	
 }
--- src/pack/Node.java
+++ src/pack/Node.java
@@ -1,5 +1,37 @@
 package pack;
 
-public class Node {
+import java.util.ArrayList;
 
+public class Node {
+    
+    //where we were before the current node. Is null by default
+    Node parent;
+    //what is stored in the node
+    RubiksCube self;
+    //the children of the node
+    ArrayList<Node> children;
+    
+    Node(RubiksCube s){
+        parent = null;
+        self = s;
+        children = new ArrayList<>();
+    }
+    
+    public void addChild(Node c){
+        c.setParent(this);
+        children.add(c);
+    }
+    
+    public void setParent(Node p){
+        parent = p;
+    }
+    
+    public Node getParent(){
+        return parent;
+    }
+    
+    public RubiksCube getSelf(){
+        return self;
+    }
+    
 }
