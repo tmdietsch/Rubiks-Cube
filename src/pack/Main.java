7ab4257de5e8d07a9b9b7d1c323995076237c52c
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-03-19 01:48:30 -0400

    New main

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -5,17 +5,84 @@ public class Main {
     	
     	RubiksCube reference = RubiksCube.REFERENCE;
     	RubiksCube cube = new RubiksCube();
+    	AStarSearch aStar;
+    	BreadthFirstSearch bfs;
     	
-    	cube.randomize(13);
-    	
+    	System.out.println("Random move selected 2 times");
+    	cube.randomize(2);
     	cube.printCube();
     	
-    	AStarSearch aStar = new AStarSearch(cube);
+    	System.out.println("Breadth First Search");
+    	bfs = new BreadthFirstSearch(cube, reference);
+    	bfs.Start();
     	
+    	System.out.println("A* Search");
+    	aStar = new AStarSearch(cube);
     	aStar.startSearch();
     	
-    	System.out.println("Done");
+    	cube = new RubiksCube();
+    	System.out.println("Random move selected 3 times");
+    	cube.randomize(3);
+    	cube.printCube();
     	
+    	System.out.println("Breadth First Search");
+    	bfs = new BreadthFirstSearch(cube, reference);
+    	bfs.Start();
     	
+    	System.out.println("A* Search");
+    	aStar = new AStarSearch(cube);
+    	aStar.startSearch();
+    	
+    	cube = new RubiksCube();
+    	System.out.println("Random move selected 4 times");
+    	cube.randomize(4);
+    	cube.printCube();
+    	
+    	System.out.println("Breadth First Search");
+    	bfs = new BreadthFirstSearch(cube, reference);
+    	bfs.Start();
+    	
+    	System.out.println("A* Search");
+    	aStar = new AStarSearch(cube);
+    	aStar.startSearch();
+    	
+    	cube = new RubiksCube();
+    	System.out.println("Random move selected 5 times");
+    	cube.randomize(5);
+    	cube.printCube();
+    	
+    	System.out.println("Breadth First Search");
+    	bfs = new BreadthFirstSearch(cube, reference);
+    	bfs.Start();
+    	
+    	System.out.println("A* Search");
+    	aStar = new AStarSearch(cube);
+    	aStar.startSearch();
+    	
+    	cube = new RubiksCube();
+    	System.out.println("Random move selected 6 times");
+    	cube.randomize(6);
+    	cube.printCube();
+    	
+    	System.out.println("Breadth First Search");
+    	bfs = new BreadthFirstSearch(cube, reference);
+    	bfs.Start();
+    	
+    	System.out.println("A* Search");
+    	aStar = new AStarSearch(cube);
+    	aStar.startSearch();
+    	
+    	cube = new RubiksCube();
+    	System.out.println("Random move selected 7 times");
+    	cube.randomize(7);
+    	cube.printCube();
+    	
+    	System.out.println("Breadth First Search");
+    	bfs = new BreadthFirstSearch(cube, reference);
+    	bfs.Start();
+    	
+    	System.out.println("A* Search");
+    	aStar = new AStarSearch(cube);
+    	aStar.startSearch();
     }
 }
