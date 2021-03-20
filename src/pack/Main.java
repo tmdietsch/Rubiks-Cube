ab91c5b5501a1b5337be4b1d02c12d3de0cd8411
Timothy Dietsch <dietschtm18@gcc.edu>
2021-03-19 21:49:11 -0400

    Added timing as condensed parsing in main

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,134 +1,47 @@
 package pack;
 
+import java.util.Timer;
+
 public class Main {
     public static void main(String[] args) {
     	//Used to ID the goal state
     	RubiksCube reference = RubiksCube.REFERENCE;
     	//the cube we will run the search algorithm on 
-    	RubiksCube cube = new RubiksCube();
+    	RubiksCube cube;;
     	//Performs the A* search
     	AStarSearch aStar;
     	//Performs the Breadth First search
     	BreadthFirstSearch bfs;
     	
-    	//Run both algorithms after making 2 moves on the cube
-    	System.out.println("Random move selected 2 times");
-    	cube.randomize(2);
-    	cube.printCube();
+    	//Timer variables
+    	double startTime, endTime;
     	
-    	System.out.println("Breadth First Search");
-    	bfs = new BreadthFirstSearch(cube, reference);
-    	bfs.Start();
-    	
-    	System.out.println("A* Search");
-    	aStar = new AStarSearch(cube);
-    	aStar.startSearch();
-    	
-    	//Run both algorithms after making 3 moves on the cube
-    	cube = new RubiksCube();
-    	System.out.println("Random move selected 3 times");
-    	cube.randomize(3);
-    	cube.printCube();
-    	
-    	System.out.println("Breadth First Search");
-    	bfs = new BreadthFirstSearch(cube, reference);
-    	bfs.Start();
-    	
-    	System.out.println("A* Search");
-    	aStar = new AStarSearch(cube);
-    	aStar.startSearch();
-    	
-    	//Run both algorithms after making 4 moves on the cube
-    	cube = new RubiksCube();
-    	System.out.println("Random move selected 4 times");
-    	cube.randomize(4);
-    	cube.printCube();
-    	
-    	System.out.println("Breadth First Search");
-    	bfs = new BreadthFirstSearch(cube, reference);
-    	bfs.Start();
-    	
-    	System.out.println("A* Search");
-    	aStar = new AStarSearch(cube);
-    	aStar.startSearch();
-    	
-    	//Run both algorithms after making 5 moves on the cube
-    	cube = new RubiksCube();
-    	System.out.println("Random move selected 5 times");
-    	cube.randomize(5);
-    	cube.printCube();
-    	
-    	System.out.println("Breadth First Search");
-    	bfs = new BreadthFirstSearch(cube, reference);
-    	bfs.Start();
-    	
-    	System.out.println("A* Search");
-    	aStar = new AStarSearch(cube);
-    	aStar.startSearch();
-    	
-    	//Run both algorithms after making 6 moves on the cube
-    	//Commented BFS out as it begins to take an unreasonable amount of time
-    	cube = new RubiksCube();
-    	System.out.println("Random move selected 6 times");
-    	cube.randomize(6);
-    	cube.printCube();
-    	
-//    	System.out.println("Breadth First Search");
-//    	bfs = new BreadthFirstSearch(cube, reference);
-//    	bfs.Start();
-    	
-    	System.out.println("A* Search");
-    	aStar = new AStarSearch(cube);
-    	aStar.startSearch();
-    	
-    	
-    	//Run both algorithms after making 7 moves on the cube
-    	//Commented BFS out as it takes an unreasonable amount of time
-    	cube = new RubiksCube();
-    	System.out.println("Random move selected 7 times");
-    	cube.randomize(7);
-    	cube.printCube();
-    	
-//    	System.out.println("Breadth First Search");
-//    	bfs = new BreadthFirstSearch(cube, reference);
-//    	bfs.Start();
-    	
-    	System.out.println("A* Search");
-    	aStar = new AStarSearch(cube);
-    	aStar.startSearch();
-    	
-    	//Run A* after making 8 moves on the cube
-    	cube = new RubiksCube();
-    	System.out.println("Random move selected 8 times");
-    	cube.randomize(8);
-    	cube.printCube();
-    	
-    	
-    	System.out.println("A* Search");
-    	aStar = new AStarSearch(cube);
-    	aStar.startSearch();
-    	
-    	//Run A* after making 9 moves on the cube
-    	cube = new RubiksCube();
-    	System.out.println("Random move selected 9 times");
-    	cube.randomize(9);
-    	cube.printCube();
-    	
-    	
-    	System.out.println("A* Search");
-    	aStar = new AStarSearch(cube);
-    	aStar.startSearch();
-    	
-    	//Run A* after making 10 moves on the cube
-    	//Commented out due to frequently unreasonable run times
-//    	cube = new RubiksCube();
-//    	System.out.println("Random move selected 10 times");
-//    	cube.randomize(10);
-//    	cube.printCube();
-//    	
-//    	
-//    	System.out.println("A* Search");
-//    	aStar = new AStarSearch(cube);
-//    	aStar.startSearch();
+    	for (int i = 2; i <= 9; i++) {
+        	System.out.println("Random move selected " + i + " times");
+        	cube = new RubiksCube();
+        	cube.randomize(i);
+        	cube.printCube();
+        	
+        	if (i <= 5) {
+	        	System.out.println("Breadth First Search");
+	        	bfs = new BreadthFirstSearch(cube, reference);
+	        	startTime = System.currentTimeMillis();
+	        	bfs.Start();
+	        	endTime = (System.currentTimeMillis() - startTime) / 1000;
+	        	
+	        	System.out.println(endTime + " seconds");
+	        	System.out.println();
+        	}
+        	
+        	System.out.println("A* Search");
+        	aStar = new AStarSearch(cube);
+        	startTime = System.currentTimeMillis();
+        	aStar.startSearch();
+        	endTime = (System.currentTimeMillis() - startTime) / 1000;
+        	
+        	System.out.println(endTime + " seconds");
+        	System.out.println();
+    	}
+
     }
 }
